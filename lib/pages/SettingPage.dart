eab0d28e04133789ff57b948cc69e53c7254ff40
K-shir0 <kakishiro2190394@gmail.com>
2020-12-16 17:12:59 +0900

    feat: 設定ページを追加

--- lib/pages/SettingPage.dart
+++ lib/pages/SettingPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SettingPage extends HookWidget {
   @override
@@ -10,10 +11,133 @@ class SettingPage extends HookWidget {
         title: new Text('設定'),
         centerTitle: true,
       ),
-      body: Container(
-
-      )
+      body: ListView(
+        padding: EdgeInsets.only(top: 1, bottom: 1),
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 12, bottom: 12),
+            child: GestureDetector(
+              onTap: () {
+                print("ヘルプボタンが押された");
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    border: const Border(
+                  top: const BorderSide(
+                    color: const Color(0xffF2F2F2),
+                    width: 1,
+                  ),
+                  bottom: const BorderSide(
+                    color: const Color(0xffF2F2F2),
+                    width: 1,
+                  ),
+                )),
+                height: 64,
+                // color: Colors.amber[600],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      flex: 9,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 32, right: 12),
+                            child: Container(
+                              width: 50,
+                              child: Center(
+                                  child:
+                                      SvgPicture.asset('assets/svg/help.svg')),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Text(
+                              "ヘルプ",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.only(right: 8, left: 16),
+                              child: SvgPicture.asset('assets/svg/next.svg'))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 12, bottom: 12),
+            child: GestureDetector(
+              onTap: () {
+                print("お気に入りボタンが押された");
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    border: const Border(
+                  top: const BorderSide(
+                    color: const Color(0xffF2F2F2),
+                    width: 1,
+                  ),
+                  bottom: const BorderSide(
+                    color: const Color(0xffF2F2F2),
+                    width: 1,
+                  ),
+                )),
+                height: 64,
+                // color: Colors.amber[600],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      flex: 9,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 32, right: 12),
+                            child: Container(
+                              width: 50,
+                              child: Center(
+                                  child:
+                                      SvgPicture.asset('assets/svg/favorite.svg')),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Text(
+                              "お気に入り",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.only(right: 8, left: 16),
+                              child: SvgPicture.asset('assets/svg/next.svg'))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
-
 }
