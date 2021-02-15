aa408fdbc2c62790e4ce7c83bb045de41bef632a
K-shir0 <kakishiro2190394@gmail.com>
2021-02-15 19:49:45 +0900

    refactor: 設定ボタンのコンポーネントを一元化した

--- lib/pages/SettingPage.dart
+++ lib/pages/SettingPage.dart
@@ -4,6 +4,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
 
 class SettingPage extends HookWidget {
+  final textStyle = TextStyle(fontSize: 14);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +16,94 @@ class SettingPage extends HookWidget {
       body: ListView(
         padding: EdgeInsets.only(top: 1, bottom: 1),
         children: [
-          _HelpButtonItem(),
-          _FavoriteButtonItem()
+          _UsageButtonItem(
+            title: Text(
+              "お気に入り",
+              style: textStyle,
+            ),
+            svgPicture: SvgPicture.asset('assets/svg/favorite.svg'),
+            onTapHandler: () {
+              print("お気に入りボタンが押された");
+              Navigator.of(context).pushNamed('/favorite');
+            },
+          ),
+          _UsageButtonItem(
+            title: Text(
+              "ヘルプ",
+              style: textStyle,
+            ),
+            svgPicture: SvgPicture.asset('assets/svg/help.svg'),
+            onTapHandler: () {
+              print("ヘルプボタンが押された");
+            },
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class _UsageButtonItem extends HookWidget {
+  final Text title;
+  final SvgPicture svgPicture;
+  final Function onTapHandler;
+
+  _UsageButtonItem(
+      {@required this.title,
+      @required this.svgPicture,
+      @required this.onTapHandler});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 12, bottom: 12),
+      child: GestureDetector(
+        onTap: onTapHandler,
+        child: Container(
+          decoration: BoxDecoration(
+              border: const Border(
+            top: const BorderSide(
+              color: const Color(0xffF2F2F2),
+              width: 1,
+            ),
+            bottom: const BorderSide(
+              color: const Color(0xffF2F2F2),
+              width: 1,
+            ),
+          )),
+          height: 64,
+          // color: Colors.amber[600],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                flex: 9,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 16, right: 12),
+                      child: Container(
+                        width: 50,
+                        child: Center(child: svgPicture),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(right: 10), child: title),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.only(right: 8, left: 16),
+                        child: SvgPicture.asset('assets/svg/next.svg'))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -34,15 +121,15 @@ class _HelpButtonItem extends StatelessWidget {
         child: Container(
           decoration: BoxDecoration(
               border: const Border(
-                top: const BorderSide(
-                  color: const Color(0xffF2F2F2),
-                  width: 1,
-                ),
-                bottom: const BorderSide(
-                  color: const Color(0xffF2F2F2),
-                  width: 1,
-                ),
-              )),
+            top: const BorderSide(
+              color: const Color(0xffF2F2F2),
+              width: 1,
+            ),
+            bottom: const BorderSide(
+              color: const Color(0xffF2F2F2),
+              width: 1,
+            ),
+          )),
           height: 64,
           // color: Colors.amber[600],
           child: Row(
@@ -57,8 +144,7 @@ class _HelpButtonItem extends StatelessWidget {
                       child: Container(
                         width: 50,
                         child: Center(
-                            child:
-                            SvgPicture.asset('assets/svg/help.svg')),
+                            child: SvgPicture.asset('assets/svg/help.svg')),
                       ),
                     ),
                     Padding(
@@ -87,73 +173,4 @@ class _HelpButtonItem extends StatelessWidget {
       ),
     );
   }
-}
-
-class _FavoriteButtonItem extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 12, bottom: 12),
-      child: GestureDetector(
-        onTap: () {
-          print("お気に入りボタンが押された");
-          Navigator.of(context).pushNamed('/favorite');
-        },
-        child: Container(
-          decoration: BoxDecoration(
-              border: const Border(
-                top: const BorderSide(
-                  color: const Color(0xffF2F2F2),
-                  width: 1,
-                ),
-                bottom: const BorderSide(
-                  color: const Color(0xffF2F2F2),
-                  width: 1,
-                ),
-              )),
-          height: 64,
-          // color: Colors.amber[600],
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                flex: 9,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 16, right: 12),
-                      child: Container(
-                        width: 50,
-                        child: Center(
-                            child:
-                            SvgPicture.asset('assets/svg/favorite.svg')),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 10),
-                      child: Text(
-                        "お気に入り",
-                        style: TextStyle(fontSize: 14),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Flexible(
-                flex: 1,
-                child: Row(
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(right: 8, left: 16),
-                        child: SvgPicture.asset('assets/svg/next.svg'))
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
 }
