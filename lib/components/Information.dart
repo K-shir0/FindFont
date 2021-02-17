07fb4ea5fca1b85f472a7ffef2a863a04ac8a231
K-shir0 <kakishiro2190394@gmail.com>
2021-02-18 02:34:24 +0900

    feat: UsagePageの雛形完成

--- lib/components/Information.dart
+++ lib/components/Information.dart
@@ -3,16 +3,32 @@ import 'package:flutter/src/widgets/framework.dart';
 
 class Information extends StatelessWidget {
   final String title;
+  final Image image;
+  final String contents;
 
-  Information({@required this.title});
+  Information(
+      {@required this.title, @required this.contents, @required this.image});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-          title: new Text(title),
-          centerTitle: true,
-        ),
-        body: Container());
+      appBar: new AppBar(
+        title: new Text(title),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          image,
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: RichText(
+              text: TextSpan(children: [
+                TextSpan(text: contents, style: TextStyle(color: Colors.black))
+              ]),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
